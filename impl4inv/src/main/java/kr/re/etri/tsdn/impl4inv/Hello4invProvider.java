fa9141abc6c2abeb3baae9908d18bd997f607e1c
justin.labry <justin.labry@gmail.com>
2015-07-24 00:33:44 +0900

    first commit

---
+++ impl4inv/src/main/java/kr/re/etri/tsdn/impl4inv/Hello4invProvider.java
@@ -0,0 +1,64 @@
+/*
+ * ETRI@copy 2015 all rights reserved. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package kr.re.etri.tsdn.impl4inv;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.DataChangeListener;
+import org.opendaylight.controller.md.sal.common.api.data.AsyncDataBroker.DataChangeScope;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
+import org.opendaylight.controller.sal.binding.api.BindingAwareProvider;
+import org.opendaylight.controller.sal.binding.api.NotificationProviderService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.hello.impl4inv.rev141210.Hello4invRuntimeMXBean;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.hello.impl4inv.rev141210.HelloImpl4invService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.hello.rev150105.HelloService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.hello.rev150105.HelloWorld;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Hello4invProvider implements BindingAwareProvider, AutoCloseable, Hello4invRuntimeMXBean {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Hello4invProvider.class);
+    private DataBroker db;
+    private NotificationProviderService notificationService;
+    private ListenerRegistration<org.opendaylight.yangtools.yang.binding.NotificationListener> listenerRegistration;
+
+    @Override
+    public void onSessionInitiated(ProviderContext session) {
+    	HelloWorldImpl helloWorldImpl = new HelloWorldImpl();
+    	
+		session.addRpcImplementation(HelloService.class, helloWorldImpl);
+		db = session.getSALService(DataBroker.class);
+		notificationService = session.getSALService(NotificationProviderService.class);
+		helloWorldImpl.setDB(db);
+		helloWorldImpl.setNotificationService(notificationService);
+		
+        listenerRegistration = notificationService.registerNotificationListener(helloWorldImpl);
+		
+        final InstanceIdentifier<HelloWorld> path = helloWorldImpl.HELLO_IID;
+        final ListenerRegistration<DataChangeListener> dataChangeListenerRegistration = 
+                        db.registerDataChangeListener(LogicalDatastoreType.CONFIGURATION, 
+                                        path, helloWorldImpl, DataChangeScope.ONE);
+		
+        LOG.info("HelloProvider Session Initiated");
+    }
+
+    @Override
+    public void close() throws Exception {
+        LOG.info("HelloProvider Closed");
+    }
+
+	@Override
+	public void updateSbNode(String id) {
+		// TODO Auto-generated method stub
+		LOG.info("hello4inv jmx updateSbNode called");
+	}
+
+}
---
+++ impl4inv/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/hello/impl4inv/rev141210/Hello4invModule.java
@@ -0,0 +1,28 @@
+package org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.hello.impl4inv.rev141210;
+
+import kr.re.etri.tsdn.impl4inv.Hello4invProvider;
+
+public class Hello4invModule extends org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.hello.impl4inv.rev141210.AbstractHello4invModule {
+    public Hello4invModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+        super(identifier, dependencyResolver);
+    }
+
+    public Hello4invModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.hello.impl4inv.rev141210.Hello4invModule oldModule, java.lang.AutoCloseable oldInstance) {
+        super(identifier, dependencyResolver, oldModule, oldInstance);
+    }
+
+    @Override
+    public void customValidation() {
+        // add custom validation form module attributes here.
+    }
+
+    @Override
+    public java.lang.AutoCloseable createInstance() {
+        // TODO:implement
+        Hello4invProvider provider = new Hello4invProvider();
+        getBrokerDependency().registerProvider(provider);
+        Hello4invRuntimeRegistration r = getRootRuntimeBeanRegistratorWrapper().register(provider);
+        return provider;
+    }
+
+}
